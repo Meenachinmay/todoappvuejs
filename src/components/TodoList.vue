1c5e88393790b44fa17a1a00d9edad1bcb263fc7
meenachinmay <chinmayanand896@gmail.com>
2019-11-11 01:06:33 +0530

    UI updated

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
-        <input type="text" class="todo-input" placeholder="What needs to be done...!" v-model="newTodo" @keyup.enter="addNewTodo">
-        <div v-for="todo in todos" :key="todo.id" class="todo-item">
-            {{ todo.title }}
+        <input type="text" class="inline-block border-2 border-gray-300 w-full text-lg px-5 py-1 rounded hover:border-gray-500 mb-5 focus:outline-none" 
+            placeholder="What needs to be done...!" v-model="newTodo" @keyup.enter="addNewTodo">
+        
+        <div v-for="todo in todos" :key="todo.id" class="flex mb-4 justify-between items-center text-xl text-gray-700 antialiased">
+          <div class="w-full">
+              <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="border border-white">{{ todo.title }}</div>
+              <input v-else type="text" class="inline-block border-2 border-gray-300 w-full text-lg px-5 py-1 rounded hover:border-gray-500 focus:outline-none" 
+                v-model="todo.title"
+                @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" v-focus>
+          </div>
+          
+          <div class="cursor-pointer mx-14 hover:text-gray-900 ml-3" @click="removeTodo(key)">
+              &times;
+          </div>
         </div>
+
     </div>
 </template>
 
@@ -18,23 +30,37 @@ export default {
             {
                 'id': 1,
                 'title': 'Finish vue project',
-                'completed': false
+                'completed': false,
+                'editing': false
             },
             {
                 'id': 2,
                 'title': 'Finish vue project',
-                'completed': false
+                'completed': false,
+                'editing': false
             },
             {
                 'id': 3,
                 'title': 'Finish vue project',
-                'completed': false
+                'completed': false,
+                'editing': false
             }
           ],
       }
   },
+  directives: {
+      focus: {
+          inserted: function(el){
+              el.focus()
+          }
+      }
+  },
   methods: {
       addNewTodo(){
+          if (this.newTodo.trim().length == 0){
+              return;
+          }
+
           this.todos.push({
               id: this.idForTodo,
               title: this.newTodo,
@@ -43,21 +69,21 @@ export default {
 
           this.newTodo = '';
           this.idForTodo++;
+      },
+      removeTodo(index){
+        this.todos.splice(index, 1);
+      },
+      editTodo(todo){
+          todo.editing = true;
+      },
+      doneEdit(todo){
+          todo.editing = false;
       }
   }
 }
-</script>a
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-    .todo-input{
-        width: 100%;
-        padding: 10px 18px;
-        font-size: 18px;
-        margin-bottom: 16px;
-        
-        &:focus {
-            outline: 0px;
-        }
-    }
+   
 </style>
