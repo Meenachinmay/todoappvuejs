70540ea9ae12dbdbe19d20da9146ffd4e1b6fe56
meenachinmay <chinmayanand896@gmail.com>
2019-11-10 16:53:27 +0530

    intiail commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <input type="text" class="todo-input" placeholder="What needs to be done...!" v-model="newTodo" @keyup.enter="addNewTodo">
+        <div v-for="todo in todos" :key="todo.id" class="todo-item">
+            {{ todo.title }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data (){
+      return {
+          newTodo: '',
+          idForTodo: '4',
+          todos: [
+            {
+                'id': 1,
+                'title': 'Finish vue project',
+                'completed': false
+            },
+            {
+                'id': 2,
+                'title': 'Finish vue project',
+                'completed': false
+            },
+            {
+                'id': 3,
+                'title': 'Finish vue project',
+                'completed': false
+            }
+          ],
+      }
+  },
+  methods: {
+      addNewTodo(){
+          this.todos.push({
+              id: this.idForTodo,
+              title: this.newTodo,
+              completed: false
+          })
+
+          this.newTodo = '';
+          this.idForTodo++;
+      }
+  }
+}
+</script>a
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+    .todo-input{
+        width: 100%;
+        padding: 10px 18px;
+        font-size: 18px;
+        margin-bottom: 16px;
+        
+        &:focus {
+            outline: 0px;
+        }
+    }
+</style>
